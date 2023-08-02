ecb949c8814f9f3eb63e1a2f34157f08ede70596
Steve Gambino <steve.gambino@cengage.com>
2023-08-02 16:50:00 -0400

    Fix script

--- jenkins-packer-demo/scripts/jenkins-run-terraform.sh
+++ jenkins-packer-demo/scripts/jenkins-run-terraform.sh
@@ -2,7 +2,6 @@
 set -ex
 cd jenkins-packer-demo
 AWS_REGION="us-east-1"
-cd jenkins-packer-demo
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
