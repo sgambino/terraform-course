5b7a133467ddeeffb2787be0c94871a46cfed71c
sgambino <stevegambino@gmail.com>
2023-08-11 18:44:09 -0400

    Update backend.tf

--- docker-demo-3/backend.tf
+++ docker-demo-3/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-2023cdef99"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-state-2023cdef99"
+#    key    = "terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
